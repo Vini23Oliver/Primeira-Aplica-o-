dab3d06c723aeda3bafd6ca4f901e02141e66b16
Vini23Oliver <vin_vinicius_@hotmail.com>
2021-04-05 13:36:44 -0300

    commit inicial

---
+++ Conta.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIO.Bank
+{
+    class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+
+        public bool saque(double valorSaque) 
+        {
+            if (this.Saldo - valorSaque < (this.Credito * -1))
+            {
+                Console.WriteLine("Saldo insuficiente! ");
+                return false;
+            }
+            else
+            {
+                this.Saldo = this.Saldo - valorSaque;
+                return true;
+
+                Console.WriteLine(" o saldo atual de conta de {0} é {1} ", this.Nome, this.Saldo);
+            }
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo = this.Saldo + valorDeposito;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", this.Nome, this.Saldo);
+        }
+
+        public void transferir(double valorTransferencia, Conta contaDestino) 
+        {
+            if (this.saque(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+
+        }
+
+        public override string ToString() // override = sobrescreve minha string
+        {
+            string retorno = "";
+            retorno = retorno + "TipoConta: " + this.TipoConta + " | ";
+            retorno = retorno + "Nome: " + this.Nome + " | ";
+            retorno = retorno + "Saldo: " + this.Saldo + " | ";
+            retorno = retorno + "Credito: " + this.Credito;
+            return retorno;
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DIO.Bank
+{
+    class Program
+    {
+        static list<Conta> ListContas = new list<Conta>();
+        private static object listContas;
+
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpçãoUsuario();// chamando minha função menu de escolha
+
+            // looping para executar os serviços de acordo com o que o usuario desejar até ser digitado "X"
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                // Validar a lista de serviços com as funçoes 
+                switch(opcaoUsuario) 
+                {
+                    case "1":
+                        ListarContas();
+                        break;
+
+                    case "2":
+                        InserirConta();
+                        break;
+
+                    case "3":
+                        //Transferir();
+                        break;
+
+                    case "4":
+                       // Sacar();
+                        break;
+
+                    case "5":
+                        //Depositar();
+                        break;
+
+                    case "C":
+                        Console.Clear(); // comando de limpar tela
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException(); // jogar(Throw) novo (new)  argumento esta fora do intervalo de exceção (ArgumentOutOfRangeException)
+
+                }
+
+                opcaoUsuario = ObterOpçãoUsuario(); // chamando de novo minha função menu 
+
+            }
+
+            Console.WriteLine("Obrigado por utilizar nossos serviços");
+        }
+
+        private static void ListarContas()
+        {
+            Console.WriteLine("Listar contas");
+
+            if (listContas.Count == 0)
+            {
+                Console.WriteLine("Nenhuma conta cadastrada.");
+                return;
+            }
+
+            for (int i = 0; i < list.Contas; i++)
+            {
+                Conta conta = list.Contas[i];
+                Console.WriteLine("#{0} - ", i);
+                Console.WriteLine(conta);
+            }
+        }
+
+        private static void InserirConta()
+        {
+            Console.WriteLine(" inserir nova conta");
+
+            Console.WriteLine("Digite 1 para conta fisica ou 2 para conta juridica: ");
+            int entradaTipoConta = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome do cliente: ");
+            string entradaNome = Console.ReadLine();
+
+            Console.WriteLine("Digite o saldo inicial: ");
+            double entradaSaldo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o credito: ");
+            double entradaCredito = Convert.ToDouble(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta, saldo: entradaSaldo, credito: entradaCredito, nome: entradaNome);
+
+            ListContas.Add(novaConta);
+        }
+
+        // criação do meu menu de escolha 
+        private static string ObterOpçãoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("DIO Bank a seu dispor!");
+            Console.WriteLine("Informe a opção desejada");
+
+            Console.WriteLine("1 - Listar Conta");
+            Console.WriteLine("2 - Inserir nova conta");
+            Console.WriteLine("3 - Transferir");
+            Console.WriteLine("4 - Sacar");
+            Console.WriteLine("5 - Depositar");
+            Console.WriteLine("C - Limpar Tela");
+            Console.WriteLine("X - Sair");
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+
+        }
+       
+    }
+}
